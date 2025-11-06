99e2873f3090cd8b4d9648e2526b324fc6454fef
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2025-11-06 13:51:06 +0100

    interfaz grafica agregada

---
+++ IGraficasIES/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IGraficasIES
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
