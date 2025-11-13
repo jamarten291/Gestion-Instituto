74eab7ba76524fe8ef9f82d5ee627084c60ffbe0
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2025-11-13 14:34:56 +0100

    add read file functionality

--- IGraficasIES/MainWindow.xaml.cs
+++ IGraficasIES/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using _2HerenciaSimpleIES.Clases;
+using Microsoft.Win32;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,12 +19,17 @@ namespace IGraficasIES
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string rutaFija = "..\\..\\..\\Imagenes\\";
+        private const string rutaFija = "..\\..\\..\\Imagenes\\";
+        private List<Persona> listaPersonas;
+        private int personaIndex = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeElements();
             InitializeImageButtons();
+            gridCentral.IsEnabled = false;
+            gridBotones.IsEnabled = false;
         }
 
         private void InitializeElements()
@@ -52,33 +59,65 @@ namespace IGraficasIES
 
         private void UpdateInterface()
         {
-
+            Persona persona = listaPersonas[personaIndex];
         }
 
         // Archivo
         private void Abrir_Click(object sender, RoutedEventArgs e)
         {
-        }
+            OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            //Nos situamos en el directorio desde el que se ejecuta la aplicación
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            //Por defecto, cuando se abra, nos va a mostrar los que sean de texto
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == true) //Cuando el usuario le dé a Aceptar
+            {
+                try
+                { //Obtengo una colección de líneas (en cada una están los datos
+                  //de un profesor), luego las iré recorriendo con un foreach
+                    var lineas = File.ReadLines(openFileDialog.FileName);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(
+                        "Lo sentimos, algo ha ido mal al leer el fichero\n" +
+                        "Quizás el fichero no existe o no tiene el formato adecuado", 
+                        "ERROR", 
+                        MessageBoxButton.OK, 
+                        MessageBoxImage.Error
+                    );
+                }
+            }
+                
+
+            } 
 
         private void Salir_Click(object sender, RoutedEventArgs e)
         {
+            Close();
         }
 
         // Fuente
         private void Negrita_Checked(object sender, RoutedEventArgs e)
         {
+            this.FontWeight = FontWeights.Bold;
         }
 
         private void Negrita_Unchecked(object sender, RoutedEventArgs e)
         {
+            this.FontWeight = FontWeights.Regular;
         }
 
         private void Cursiva_Checked(object sender, RoutedEventArgs e)
         {
+            this.FontStyle = FontStyles.Italic;
         }
 
         private void Cursiva_Unchecked(object sender, RoutedEventArgs e)
         {
+            this.FontStyle = FontStyles.Normal;
         }
 
         // Filtros
@@ -159,22 +198,40 @@ namespace IGraficasIES
 
         private void BtnPrimero_Click(object sender, RoutedEventArgs e)
         {
+            btnPrimero.IsEnabled = ComprobarBotonActivo((Button)sender);
+            personaIndex = 0;
             UpdateInterface();
         }
 
         private void BtnAnterior_Click(object sender, RoutedEventArgs e)
         {
+            btnAnterior.IsEnabled = ComprobarBotonActivo((Button)sender);
+            personaIndex--;
             UpdateInterface();
         }
 
         private void BtnSiguiente_Click(object sender, RoutedEventArgs e)
         {
+            btnSiguiente.IsEnabled = ComprobarBotonActivo((Button)sender);
+            personaIndex++;
             UpdateInterface();
         }
 
         private void BtnUltimo_Click(object sender, RoutedEventArgs e)
         {
+            btnUltimo.IsEnabled = ComprobarBotonActivo((Button) sender);
+            personaIndex = listaPersonas.Count - 1;
             UpdateInterface();
+        }
+
+        private bool ComprobarBotonActivo(Button b)
+        {
+            return b.Name switch
+            {
+                "btnAnterior" or "btnPrimero" => personaIndex != 0,
+                "btnSiguiente" or "btnUltimo" => personaIndex != (listaPersonas.Count - 1),
+                _ => false,
+            };
         }
     }
 }
